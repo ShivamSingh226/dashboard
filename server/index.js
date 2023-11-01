d43f15d9202bb8b324b16167857a8cd0c1b09890
ShivamSingh226 <shivamsingh226@gmail.com>
2023-11-01 14:17:16 +0530

    Started building dashboard

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors"
+import dotenv from "dotenv"
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js"
+import generalRoutes from "./routes/general.js"
+import salesRoutes from "./routes/sales.js"
+import managementRoutes from "./routes/management.js"
+import mongoose from "mongoose";
+
+dotenv.config();
+const app=express();
+app.use(express.json())
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+app.use("/client",clientRoutes);
+app.use("/general",generalRoutes);
+app.use("/sales",salesRoutes);
+app.use("/management",managementRoutes);
+
+const PORT=process.env.PORT||9000;
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    app.listen(PORT,()=>console.log(`Server port : ${PORT}`));
+}).catch((error)=>console.log(`${error} did not connect`));
