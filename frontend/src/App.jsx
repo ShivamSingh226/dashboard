38a5961752860a2a2cd516148e3d2f3e6357e81a
ShivamSingh226 <shivamsingh226@gmail.com>
2023-11-09 14:07:33 +0530

    Added customers routes and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter,Navigate,Route,Routes } from "react-router-dom"
 import { themeSettings } from "./theme"
 import Dashboard from "./scenes/dashboard"
 import Layout from "./scenes/layout"
+import Products from "./scenes/products"
+import Customers from "./scenes/customers"
 function App() {
         const mode=useSelector((state)=>state.global.mode);
         const theme=useMemo(()=>createTheme(themeSettings(mode)),[mode]);
@@ -19,6 +21,8 @@ function App() {
               <Route element={<Layout/>}>
                   <Route path="/" element={<Navigate to="/dashboard" replace />}/>
                   <Route path="/dashboard" element={<Dashboard/>}/>
+                  <Route path="/products" element={<Products/>}/>
+                  <Route path="/customers" element={<Customers/>}/>
               </Route>
             </Routes>
           </ThemeProvider>
--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 export const getProducts=async(req,res)=>{
     try{
        const products=await Product.find();
@@ -18,4 +19,12 @@ export const getProducts=async(req,res)=>{
     }catch(error){
         res.status(404).json({message:error.message});
     }
+}
+export const getCustomers=async(req,res)=>{
+    try{
+       const customers=await User.find({role:"user"}).select("-password");
+       res.status(200).json(customers);
+     }catch(error){
+         res.status(404).json({message:error.message});
+     }
 }
